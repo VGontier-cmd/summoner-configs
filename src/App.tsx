a90b84c4ee4c87ac0693802429f9e655cbc9ea91
Gimly-76 <theo.carel99@gmail.com>
2023-05-25 12:38:47 +0200

    add check if league of legends is open with child_process

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,61 @@
-import { useState } from 'react'
-import Update from '@/components/update'
-import logoVite from './assets/logo-vite.svg'
-import logoElectron from './assets/logo-electron.svg'
+import React, { useEffect, useState } from 'react';
+import Loader from '@/components/Loader'
 import './App.scss'
 
-console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}!`)
-
 function App() {
-  const [count, setCount] = useState(0)
-  return (
-    <div className='App'>
-      <div className='logo-box'>
-        <a href='https://github.com/electron-vite/electron-vite-react' target='_blank'>
-          <img src={logoVite} className='logo vite' alt='Electron + Vite logo' />
-          <img src={logoElectron} className='logo electron' alt='Electron + Vite logo' />
-        </a>
-      </div>
-      <h1>Electron + Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Electron + Vite logo to learn more
-      </p>
-      <div className='flex-center'>
-        Place static files into the<code>/public</code> folder <img style={{ width: '5em' }} src='./node.svg' alt='Node logo' />
-      </div>
+  const [isLeagueOfLegendsOpen, setIsLeagueOfLegendsOpen] = useState(false);
+  const { exec } = require('child_process');
+  const time = 5000;
+  const execApp = 'LeagueClientUx.exe';
 
-      <Update />
+  useEffect(() => {
+
+    // On met à jour l'état d'ouverture de l'application 'League of Legends'
+    const checkLeagueOfLegendsStatus = () => {
+      return new Promise<void>((resolve, reject) => {
+        exec(`tasklist /fi "imagename eq ${execApp}"`, (error: Error | null, stdout: string, stderr: string) => {
+          if (error) {
+            reject(error);
+            return;
+          }
+
+          const isOpen = stdout.includes(execApp);
+          setIsLeagueOfLegendsOpen(isOpen);
+          resolve();
+        });
+      });
+    };
+
+    // On vérifie l'état d'ouverture de l'application 'League of Legends' toutes les 5 secondes
+    const startChecking = async () => {
+      while (true) {
+        try {
+          await checkLeagueOfLegendsStatus();
+        } catch (error) {
+          console.error(error);
+        }
+
+        await new Promise<void>((resolve) => setTimeout(resolve, time));
+      }
+    };
+
+    startChecking();
+  }, []);
+
+  return (
+    <div>
+      {isLeagueOfLegendsOpen ? (
+        <div>
+          <p>Bienvenue !</p>
+        </div>
+      ) : (
+        <>
+          <Loader />
+          <div className='loader-message'>
+            <span>En attente de l'ouverture de League of Legends...</span>
+          </div>
+        </>
+      )}
     </div>
   )
 }
