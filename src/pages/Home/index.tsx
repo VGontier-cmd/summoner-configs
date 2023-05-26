395a91e08c64100eac6d94ff3299dc08769a138c
Gimly-76 <theo.carel99@gmail.com>
2023-05-26 17:01:48 +0200

    move Home into pages folder

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+function Home() {
+  return (
+    <div className='wrapper'>
+      <h1>Bonjour !</h1>
+    </div>
+  )
+}
+
+export default Home
