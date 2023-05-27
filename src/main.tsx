e212312951a39739b1cc3590ef7562568a6ab86b
Gimly-76 <theo.carel99@gmail.com>
2023-05-27 03:05:53 +0200

    update home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import Header from '@/components/Header'
+import Header from '@/layouts/Header'
 import './samples/node-api'
 import '@/assets/stylesheets/index.scss'
 
