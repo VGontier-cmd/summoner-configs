78a8c4b22a5576764fabee9cd37e1ab705acf9d3
Théo Carel <59773310+TCarel-dev@users.noreply.github.com>
2023-06-05 16:04:08 +0200

    add dialog, dropdown-menu & cal-sans font

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,15 +9,26 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 import { Label } from '@/components/ui/label'
+import { Input } from "@/components/ui/input"
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
 import { Icons } from '@/components/Icons'
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 const Home = () => {
   const nbProfiles = 5;
 
-	const profiles = [];
-	for (let i = 0; i < nbProfiles; i++) {
-		profiles.push(
+  const profiles = [];
+  for (let i = 0; i < nbProfiles; i++) {
+    profiles.push(
       <Label
         key={i}
         htmlFor={`profile-${i}`}
@@ -25,13 +36,13 @@ const Home = () => {
       >
         <RadioGroupItem value={`profile-${i}`} id={`profile-${i}`} className='sr-only' />
         <Icons.paypal className='mb-3 h-6 w-6' />
-        Profile 2
+        Profile {i + 1}
       </Label>
-		);
-	}
+    );
+  }
 
   return (
-    <Card>
+    <Card className='font-sans'>
       <CardHeader>
         <CardTitle>Manage your profiles !</CardTitle>
         <CardDescription>
@@ -39,12 +50,38 @@ const Home = () => {
         </CardDescription>
       </CardHeader>
       <CardContent className='grid gap-6'>
+        <div className='flex justify-end'>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="secondary">Add Profile</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Add Profile</DialogTitle>
+                <DialogDescription>
+                  Add a name to your new profile.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="flex flex-col gap-3">
+                  <Label htmlFor="name">
+                    Name
+                  </Label>
+                  <Input id="name" className="col-span-3" />
+                </div>
+              </div>
+              <DialogFooter>
+                <Button type="submit">Save changes</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
         <RadioGroup defaultValue='card' className='grid grid-cols-3 gap-4'>
           {profiles}
         </RadioGroup>
       </CardContent>
       <CardFooter>
-        <Button variant='outline' className='w-full'>Export to League of Legends</Button>
+        <Button className='w-full'>Export to League of Legends</Button>
       </CardFooter>
     </Card>
   )
