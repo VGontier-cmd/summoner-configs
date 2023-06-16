ad1503b0b0a9919b21485d279512b10bde35816f
Théo Carel <59773310+TCarel-dev@users.noreply.github.com>
2023-06-16 14:03:50 +0200

    add: delete component to profiles

---
+++ src/layouts/Profiles/Delete.tsx
@@ -0,0 +1,59 @@
+import { ipcRenderer } from 'electron';
+
+import {
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+
+import { useToast } from '@/components/ui/use-toast';
+
+interface DeleteProps {
+	profileId: string;
+}
+
+const Delete = ({ profileId }: DeleteProps) => {
+	const { toast } = useToast();
+
+	const handleDeleteProfile = () => {
+		if (profileId) {
+			ipcRenderer
+				.invoke('ipcmain-profile-delete', profileId)
+				.then((result) => {
+					window.location.reload();
+					toast({
+						description: 'The profile has been deleted successfully !',
+					});
+				})
+				.catch((error) => {
+					console.error(error);
+					toast({
+						description: `Error deleting profile: ${error}`,
+					});
+				});
+		}
+	};
+
+	return (
+		<>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Are you absolutely sure ?</AlertDialogTitle>
+					<AlertDialogDescription>
+						This action cannot be undone. This will permanently delete your selected profile settings.
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel>Cancel</AlertDialogCancel>
+					<AlertDialogAction onClick={() => handleDeleteProfile()}>Continue</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</>
+	);
+};
+
+export default Delete;
