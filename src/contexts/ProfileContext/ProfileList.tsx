c0d5222c1e50371f7e50dbca37669f342443a8df
Théo Carel <59773310+TCarel-dev@users.noreply.github.com>
2023-07-06 18:58:57 +0200

    update: profileList

--- src/contexts/ProfileContext/ProfileList.tsx
+++ src/contexts/ProfileContext/ProfileList.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-
-import { ipcRenderer } from 'electron';
 import { Profile } from 'electron/main/modules/profile-manager/profile.interface';
 
 import { Icons } from '@/components/Icons';
@@ -29,69 +26,69 @@ const ProfileList = ({ selectedProfileId, handleProfileClick }: ListProfileProps
 	const { profiles, editingProfileId, handleEditDialogOpenChange, handleOpenProfileInFileExplorer } =
 		useProfileContext();
 
+	if (profiles?.length == 0) {
+		return (
+			<div className="text-center p-4">
+				<span className="text-sm text-light">No profile saved...</span>
+			</div>
+		);
+	}
+
 	return (
-		<>
-			{profiles?.length > 0 ? (
-				<ul className="grid grid-cols-3 gap-3 px-4 pb-[7rem] pt-5">
-					{profiles.map((profile: Profile) => (
-						<li
-							key={profile.id}
-							className="profile-item relative gold-gradient-border border-thin cursor-pointer flex flex-col items-center justify-center text-center border-muted bg-popover p-4 py-5"
-							aria-selected={selectedProfileId === profile.id ? 'true' : 'false'}
-							onClick={() => handleProfileClick(profile.id)}
-						>
-							<Dialog
-								open={editingProfileId === profile.id}
-								onOpenChange={(isOpen) => handleEditDialogOpenChange(isOpen, profile.id)}
-							>
-								<AlertDialog>
-									<DropdownMenu>
-										<DropdownMenuTrigger asChild>
-											<button className="absolute outline-none p-2 top-0 right-0 flex items-center justify-center">
-												<Icons.moreVertical className="h-4 w-4" />
-											</button>
-										</DropdownMenuTrigger>
-										<DropdownMenuContent className="w-56">
-											<DropdownMenuGroup>
-												<DialogTrigger asChild>
-													<DropdownMenuItem>
-														<Pen className="mr-2 h-4 w-4" />
-														<span>Update</span>
-													</DropdownMenuItem>
-												</DialogTrigger>
+		<ul className="grid grid-cols-3 gap-3 px-4 pb-[7rem] pt-5">
+			{profiles.map((profile: Profile) => (
+				<li
+					key={profile.id}
+					className="profile-item relative gold-gradient-border border-thin cursor-pointer flex flex-col items-center justify-center text-center border-muted bg-popover p-4 py-5"
+					aria-selected={selectedProfileId === profile.id ? 'true' : 'false'}
+					onClick={() => handleProfileClick(profile.id)}
+				>
+					<Dialog
+						open={editingProfileId === profile.id}
+						onOpenChange={(isOpen) => handleEditDialogOpenChange(isOpen, profile.id)}
+					>
+						<AlertDialog>
+							<DropdownMenu>
+								<DropdownMenuTrigger asChild>
+									<button className="absolute outline-none p-2 top-0 right-0 flex items-center justify-center">
+										<Icons.moreVertical className="h-4 w-4" />
+									</button>
+								</DropdownMenuTrigger>
+								<DropdownMenuContent className="w-56">
+									<DropdownMenuGroup>
+										<DialogTrigger asChild>
+											<DropdownMenuItem>
+												<Pen className="mr-2 h-4 w-4" />
+												<span>Update</span>
+											</DropdownMenuItem>
+										</DialogTrigger>
 
-												<DropdownMenuItem onClick={() => handleOpenProfileInFileExplorer(profile)}>
-													<Folder className="mr-2 h-4 w-4" />
-													Open Folder
-												</DropdownMenuItem>
+										<DropdownMenuItem onClick={() => handleOpenProfileInFileExplorer(profile)}>
+											<Folder className="mr-2 h-4 w-4" />
+											Open Folder
+										</DropdownMenuItem>
 
-												<AlertDialogTrigger asChild>
-													<DropdownMenuItem>
-														<Trash className="mr-2 h-4 w-4" />
-														<span>Delete</span>
-													</DropdownMenuItem>
-												</AlertDialogTrigger>
-											</DropdownMenuGroup>
-										</DropdownMenuContent>
-									</DropdownMenu>
-									<DeleteProfile profile={profile} />
-								</AlertDialog>
-								<EditProfile profile={profile} />
-							</Dialog>
+										<AlertDialogTrigger asChild>
+											<DropdownMenuItem>
+												<Trash className="mr-2 h-4 w-4" />
+												<span>Delete</span>
+											</DropdownMenuItem>
+										</AlertDialogTrigger>
+									</DropdownMenuGroup>
+								</DropdownMenuContent>
+							</DropdownMenu>
+							<DeleteProfile profile={profile} />
+						</AlertDialog>
+						<EditProfile profile={profile} />
+					</Dialog>
 
-							<Icons.lol className="mb-3 h-6 w-6" />
-							<div className="line-clamp -lc-1">
-								<p className="text-sm ">{profile.name}</p>
-							</div>
-						</li>
-					))}
-				</ul>
-			) : (
-				<div className="text-center p-4 shadow-md">
-					<span className="text-sm text-light">No profile saved...</span>
-				</div>
-			)}
-		</>
+					<Icons.lol className="mb-3 h-6 w-6" />
+					<div className="line-clamp -lc-1">
+						<p className="text-sm ">{profile.name}</p>
+					</div>
+				</li>
+			))}
+		</ul>
 	);
 };
 
